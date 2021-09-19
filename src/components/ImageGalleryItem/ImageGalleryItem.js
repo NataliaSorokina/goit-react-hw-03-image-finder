d39cfd5ee6d942eaa7b038dfbbedf2de0cbb18de
Natalia Sorokina <pticasonabird@gmail.com>
2021-09-19 22:26:32 +0300

    corrected ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function ImageGalleryItem({ src, alt, key }) {
+function ImageGalleryItem({ src, alt }) {
   return (
     <li /*  className="ImageGalleryItem" */>
       <img
         src={src}
         alt={alt}
-        id={key} /* className="ImageGalleryItem-image" */
+        /* className="ImageGalleryItem-image" */
       />
     </li>
   );
